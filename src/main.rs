0fb408b19840c300c339abc42891b6afd8e79655
royshulman <roy.shulman@gmail.com>
2022-08-26 23:56:18 +0300

    pretty much working sgd

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,49 @@ mod graph;
 mod loss_functions;
 mod nn;
 
-use graph::ScalarTensorGraph;
-use nn::MLP;
+use nn::{Module, MLP};
 
 use crate::engine::MutableScalarTensor;
 
+fn sgd(
+    n: &mut MLP,
+    inputs: &Vec<Vec<f32>>,
+    target: &Vec<f32>,
+    num_iterations: usize,
+    learning_rate: f32,
+) {
+    for epoch in 0..num_iterations {
+        // Forward pass
+        let ypreds = inputs
+            .iter()
+            .map(|x| n.scalar_forward(x))
+            .collect::<Vec<MutableScalarTensor>>();
+        let loss = loss_functions::mse_loss(&ypreds, target);
+
+        // Backward pass
+        n.zero_grad();
+        loss.backward();
+
+        // Update params
+        for p in n.parameters() {
+            let mut p = p.borrow_mut();
+            // We want the loss to go down so we add by negative grad
+            p.data += -learning_rate * p.grad;
+        }
+
+        println!("epoch: {}, loss: {}", epoch, loss.borrow().data);
+    }
+    let ypreds = inputs
+            .iter()
+            .map(|x| n.scalar_forward(x))
+            .collect::<Vec<MutableScalarTensor>>();
+    for pred in ypreds.iter() {
+        println!("{}", pred.borrow().data);
+    }
+}
+
 fn binary_classifier_example() {
-    let n = MLP::new(3, vec![4, 4, 1]);
+    let mut n = MLP::new(3, vec![4, 4, 1]);
     let xs = vec![
         vec![2.0, 3.0, -1.0],
         vec![3.0, -1.0, 0.5],
@@ -18,13 +54,7 @@ fn binary_classifier_example() {
         vec![1.0, 1.0, -1.0],
     ];
     let ys = vec![1.0, -1.0, -1.0, 1.0];
-    let ypreds = xs
-        .iter()
-        .map(|x| n.scalar_forward(x))
-        .collect::<Vec<MutableScalarTensor>>();
-
-    let loss = loss_functions::mse_loss(&ypreds, &ys);
-    println!("{}", ScalarTensorGraph::draw_root(&loss));
+    sgd(&mut n, &xs, &ys, 1000, 0.01);
 }
 
 fn main() {
